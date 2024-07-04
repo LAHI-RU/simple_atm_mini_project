d4a09ed9a2488aee0de187f0ca93d0e0a9a5ecb1
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-07-04 22:28:13 +0530

    update balance

---
+++ ATM-mini-project.py
@@ -0,0 +1,15 @@
+import time
+print("Please Insert Your ATM CARD")
+time.sleep(2)
+print("Loading.....")
+time.sleep(3)
+password = 225577
+pin_code = int(input("Please Enter Your PIN "))
+balance = 0
+if pin_code == password:
+    pass
+print("Enter Press 1 for Balance ")
+print("Enter Press 2 for Withdraw the Cash ")
+print("Enter Press 3 for Deposit the Cash ")
+print("Enter Press 4 for EXIT ")
+
