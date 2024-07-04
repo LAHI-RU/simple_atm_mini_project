18e8e016ef164d1caea3640416d3c9ce081eb145
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-07-04 22:43:37 +0530

    update

--- ATM-mini-project.py
+++ ATM-mini-project.py
@@ -13,3 +13,27 @@ print("Enter Press 2 for Withdraw the Cash ")
 print("Enter Press 3 for Deposit the Cash ")
 print("Enter Press 4 for EXIT ")
 
+try:
+    option = int(input("Please Enter Your Choice: "))
+
+except Exception as e:
+    print("Please Enter Valid Option")
+    if option == 1:
+        print(f"Your Current Balance is = {balance}")
+
+    if option == 2:
+        withdraw_amount = int(input("Please Enter Your Withdraw Amount: "))
+        balance = balance - withdraw_amount
+        print(f"{withdraw_amount} is debited from your account")
+        print(f"Your Current Balance is = {balance}")
+
+    if option == 3:
+        deposit_amount = int(input("Please Enter Your Deposit Amount: "))
+        balance = balance + deposit_amount
+        print(f"Your Updated Balance is = {balance}")
+
+    if option == 4:
+        print("Thank You! ")
+        exit()
+
+
